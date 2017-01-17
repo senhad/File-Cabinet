418e7d325bf9f8fa131b64a6d17a74d0cd995db0
Senad Hadrovic <github email address>
2017-01-16 20:51:20 -0800

    Added edit routes and form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -29,7 +29,7 @@ end
 
 get '/notes/:id' do 
 	if logged_in?
-		@note = Note.find_by_id(params[:id])
+		@note = current_user.notes.find_by_id(params[:id])
 		erb :'notes/show'
 	else 
 		redirect '/login'
@@ -38,11 +38,19 @@ end
 
 get '/notes/:id/edit' do 
 	if logged_in?
-		@note = Note.find_by_id(params[:id])
+		@note = current_user.notes.find_by_id(params[:id])
 		erb :'notes/edit'
 	else 
 		redirect '/login'
 	end 
+end 
+
+patch '/notes/:id' do 
+	@note = Note.find_by_id(params[:id])
+		@note.title = params[:title]
+		@note.content = params[:content]
+		@note.save
+		redirect "/notes/#{@note.id}"
 end 
 
 
