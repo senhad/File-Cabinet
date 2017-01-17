a252740df9316c55f04fa346d2d5c0c93f6a635c
Senad Hadrovic <github email address>
2017-01-16 18:17:17 -0800

    Added routes for '/signup' and '/login' ad well as corresponding erb files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do 
+  		erb :index
+  end 
+
+  get '/signup' do 
+  		erb :'users/signup'
+  end 
+
+  get '/login' do
+    	erb :'users/login'	
   end
+
+
+
 
 end
